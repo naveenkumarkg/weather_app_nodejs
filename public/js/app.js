224ba2cffe3141d7190b943ccbffc4560a4abfb6
Naveen <navinkumar618@gmail.com>
2021-12-20 21:04:11 +0530

    fetch api restoer

--- public/js/app.js
+++ public/js/app.js
@@ -12,8 +12,8 @@ weatherForm.addEventListener('submit', (e) => {
 
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
-
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    // http://localhost:3000/weather?address=
+    fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             console.log(data)
             if (data.error) {
